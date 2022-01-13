ea569b60a33f1b78c7a8d283d6427a3a6bb0cc14
Vytautas Palukaitis <49992307+MrGenQ@users.noreply.github.com>
2022-01-13 15:52:35 +0200

    Update functions.php

--- Text_File/inc/functions.php
+++ Text_File/inc/functions.php
@@ -15,10 +15,10 @@ function getData(){
 function validate($data){
     global $validationErrors;
     if(empty($data['name']) && !preg_match("/[a-zA-Z]/", $data['name']) || preg_match("/\\s/", $data['name'])){
-        $validationErrors[]="neįvestes arba neteisingai ivestass vardas";
+        $validationErrors[]="neįvestas arba neteisingai ivestass vardas";
     }
     if(empty($data['lname']) && !preg_match("/[a-zA-Z]/", $data['lname']) || preg_match("/\\s/", $data['lname'])){
-        $validationErrors[]="neįvestes arba neteisingai ivestas pavarde";
+        $validationErrors[]="neįvestas arba neteisingai ivestas pavarde";
     }
     if(empty($data['email']) && !preg_match("/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/",$data['email']))
     {
